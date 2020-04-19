1cd4e3191b30af60e83adb483ec5d05a56545422
qiguoqi <2280220550@qq.com>
2020-04-19 20:37:48 +0800

    419第二次提交

--- src/pages/ui/carousel.js
+++ src/pages/ui/carousel.js
@@ -6,8 +6,8 @@ class Carousels extends Component {
   render() {
     return (
       <div>
-        <Card title="图片背景轮播" className="Card">
-          <Carousel>
+        <Card title="文字背景轮播" className="Card">
+          <Carousel autoplay>
             <div>
               <h3>2</h3>
             </div>
@@ -19,6 +19,19 @@ class Carousels extends Component {
             </div>
             <div>
               <h3>4</h3>
+            </div>
+          </Carousel>
+        </Card>
+        <Card title="文字背景轮播" className="Card">
+          <Carousel autoplay>
+            <div>
+              <h3><img src="/carousel-img/carousel-1.jpg" alt=""/></h3>
+            </div>
+            <div>
+              <h3><img src="/carousel-img/carousel-2.jpg" alt=""/></h3>
+            </div>
+            <div>
+              <h3><img src="/carousel-img/carousel-3.jpg" alt=""/></h3>
             </div>
           </Carousel>
         </Card>
